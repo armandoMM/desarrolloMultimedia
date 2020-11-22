dcf48fc481a8f8e9fad23056c6074f1edcbe6b7c
Armando Matias Montaño <mando001gamer@gmail.com>
2020-11-21 21:38:26 -0600

    skelet ready

--- proy-desarrollo/src/App.vue
+++ proy-desarrollo/src/App.vue
@@ -1,28 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header>
+      <img src="../../5G/banner.png" alt="banner" class="banner" />
+    </header>
+    <myMenu />
+    <cards />
   </div>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import cards from "./components/cards.vue";
+import myMenu from "./components/my-menu.vue";
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  components: { cards, myMenu },
 };
 </script>
-
 <style>
-#app {
+body {
+  /* background-color: #ffffff;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%2380e573' fill-opacity='0.52'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E"); */
+  background-color: #ffffff;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%2324d1ae' fill-opacity='0.16'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E");
+  font-family: "Exo 2", sans-serif;
+  font-size: 96;
+}
+
+.menu {
+  padding: 20px 0px 0px 0px;
+  background-color: #88b85a;
+  width: 300px;
+  height: 600px;
+  float: left;
+}
+
+li {
+  list-style: none;
+  margin: 55px 0 0 5px;
+}
+
+.banner {
+  max-width: 1360px;
+  width: 100%;
+}
+
+.card {
+  width: auto;
+  overflow: hidden;
+}
+
+.my-card {
+  margin: 30px 0px 0px 60px;
+}
+
+.title-cont {
+  color: #4f9c7a;
+  text-align: center;
+}
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+                                                   
+#nav {
+  padding: 30px;
+}
+                                                    
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+                                                  
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
 </style>
---
+++ proy-desarrollo/src/components/my-menu.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="menu">
+    <ul>
+      <li v-for="opt in options" v-bind:key="opt">
+        <a href="">{{ opt }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {
+  name: "my-menu",
+  data() {
+    return {
+      options: [
+        "¿QUÉ ES EL 5G?",
+        "¿CÓMO FUNCIONA?",
+        "CARACTERÍSTICAS",
+        "VENTAJAS",
+        "DESVENTAJAS",
+        "5G EN MÉXICO",
+        "¿MI TELÉFONO SOPORTARÁ 5G?",
+      ],
+    };
+  },
+};
+</script>
