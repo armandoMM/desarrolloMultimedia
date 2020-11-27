f8a255e1ab9fd3a6f17d628d976887935361d3d4
Armando Matias Montaño <mando001gamer@gmail.com>
2020-11-27 13:43:17 -0600

    se agregó vistas, componentes y css

--- proy-desarrollo/src/App.vue
+++ proy-desarrollo/src/App.vue
@@ -4,29 +4,56 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  /* background-color: #ffffff;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%2380e573' fill-opacity='0.52'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E"); */
+  background-color: #ffffff;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%2324d1ae' fill-opacity='0.16'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E");
+  font-family: "Exo 2", sans-serif;
+}
+
+.menu {
+  padding: 20px 0px 0px 0px;
+  background-color: #88b85a;
+  font-size: 96;
+  width: 300px;
+  height: 600px;
+  float: left;
+}
+
+li {
+  list-style: none;
+  margin: 55px 0 0 5px;
+}
+
+.banner {
+  max-width: 1360px;
+  width: 100%;
+}
+
+.card {
+  font-size: 96;
+  width: auto;
+  overflow: hidden;
+}
+
+.my-card {
+  margin: 30px 0px 0px 60px;
+}
+
+.title-cont {
+  color: #4f9c7a;
   text-align: center;
-  color: #2c3e50;
+  font-family: "Exo 2", sans-serif;
+  font-size: 184;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.title {
+  color: #88b85a;
 }
 </style>
--- proy-desarrollo/src/views/Home.vue
+++ proy-desarrollo/src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="card">
+      <section>
+        <h1 class="title">BIENVENIDOS</h1>
+        <p>
+          En esta página encontrarás todo lo que necesitas saber acerca de la
+          red 5G.
+          <img src="../assets/images.jpg" alt="5G" />
+        </p>
+        <p>
+          Además de información tenemos audios y videos complementarios para que
+          puedas entender de forma clara a la red 5G
+        </p>
+        <p>
+          Te suguiero que sigas el orden en el que aparecen los temas en el
+          menú, para que puedas entender todo lo que sigue. Si ya tienes
+          conocimientos acerca del 5G, puedes saltarte directamente los temas
+          que te interesan.
+        </p>
+        <h3>SIN NADAMÁS QUE DECIR QUE TENGAS UN BUEN VIAJE INTERNAUTA.</h3>
+      </section>
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    //HelloWorld
+  },
+};
 </script>
